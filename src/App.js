b6a74de72480f15c382974bbe557e65b63b4c437
mnmr1996 <mnmr1996@gmail.com>
2019-03-25 14:35:51 -0400

    initalized array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    contactlist: [
+      {
+        FirstName: "Cathy",
+        LastName: "Pierce",
+        Birthday: "9/14/1996",
+        Telephone: "200-910-8132"
+      },
+      {
+        FirstName: "Alfonso",
+        LastName: "Cooley",
+        Birthday: "8/10/1973",
+        Telephone: "200-657-9362",
+        visibility: false
+      },
+      {
+        FirstName: "Victor",
+        LastName: "Gordon",
+        Birthday: "8/3/1970",
+        Telephone: "200-661-9407"
+      },
+      {
+        FirstName: "Colleen",
+        LastName: "Wright",
+        Birthday: "10/28/1967",
+        Telephone: "200-250-7949"
+      },
+      {
+        FirstName: "James",
+        LastName: "Johnston",
+        Birthday: "5/11/1972",
+        Telephone: "200-645-3176"
+      },
+      {
+        FirstName: "Anna",
+        LastName: "Reyes",
+        Birthday: "9/10/1975",
+        Telephone: "200-707-8670"
+      }
+    ]
+  };
   render() {
     return <div className="App" />;
   }
